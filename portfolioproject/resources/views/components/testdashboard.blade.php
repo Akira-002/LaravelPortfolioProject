7f18f3269492fdf53e6a778fcc55d171ada48fde
Akira-002 <alcumgmn@gmail.com>
2020-11-14 05:33:59 +0900

    Register and display 002:components /Akira

---
+++ portfolioproject/resources/views/components/testdashboard.blade.php
@@ -0,0 +1,5 @@
+<div class="p-6">
+	<h2>this is test blade of showing dashboard!!!!</h2>
+
+	<p>002 try studying Laravel 8 :)</p>
+</div>
--- portfolioproject/resources/views/welcome.blade.php
+++ portfolioproject/resources/views/welcome.blade.php
@@ -24,7 +24,8 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
+                    {{-- branch of the authentication condition --}}
+                    @auth  {{-- auth --}}
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
@@ -32,7 +33,7 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
-                    @endif
+                    @endif　{{-- end auth --}}
                 </div>
             @endif
 
